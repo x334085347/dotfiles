3af915d7faea9721713263a43bc01fa3785e9a69
xujiang <x334085347@126.com>
2019-06-12 15:30:14 +0800

    reattach-to-user-namespace

--- sync.sh
+++ sync.sh
@@ -3,8 +3,8 @@ set -e
 #bak
 files=('.vimrc' '.vim' )
 for file in ${files[@]} ;
- do echo "bak  $file" ;
  if [[ -f "~/$file" || -d "~/$file" ]]; then
+     do echo "bak  $file" ;
      mv  ~/$file ~/${file}.bak ;
  fi
 done ;
@@ -13,3 +13,4 @@ export dir_name=$(cd "$(dirname "$0")";pwd)
 ln -s -f ${dir_name}/vimrc ~/.vimrc
 ln -s -f ${dir_name}/vim  ~/.vim
 ln -s -f ${dir_name}/tmux.conf ~/.tmux.conf
+# brew install reattach-to-user-namespace #  can use pbcopy in mac system
